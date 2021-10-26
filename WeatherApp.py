010e0341a1a55ad5a419b82357e9b2978b728631
Frabj0us <61592314+Frabj0us@users.noreply.github.com>
2021-10-26 09:30:47 -0500

    Update WeatherApp.py

--- WeatherApp.py
+++ WeatherApp.py
@@ -6,17 +6,42 @@
 import WeatherInfo
 
 #Set your key
+
 WeatherInfo.setKey("6b04b92feb317046e6d27d2261c3e291")
 
 #Ask the user for their city
+
 Str city = raw_input("")
+
 #Update the weather with the given city
 
+success = WeatherInfo.updateWeather()
+
+if(success == True):
+  print("It worked")
+else:
+  print("Invalid City")
+  
 #Request any data you need from the WeatherInfo API
 
+getDescription()
+getTemp
+getFeelsLike
+getHumidity
+getPressure
+getWindSpeed
+
 #Process the data
+
+
 #convert temperature to fahrenheit,
+
+float tempF = (getTemp() -273.15) * 9/5 + 32
+
 #determine wind speed in words
+
+print("the wind speed is " + (getWindSpeed) + " mph."
+
 #decide jacket and umbrella status
 
 #Report to the user the weather of their city
